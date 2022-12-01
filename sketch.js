6fc50acc2aeb1535c7e57c9c7c66404a6da8ccd3
Goosy-Goose <35647530+Goosy-Goose@users.noreply.github.com>
2022-12-01 05:57:11 -0500

    yeah

--- sketch.js
+++ sketch.js
@@ -1,13 +1,27 @@
 var SWidth, SHeight;
 var FNameList, LNameList;
+var Conthrax, SofachromeIt, Sofachrome;
 var FName, LName;
 var StartPts;
 
 
+function preload(){
+  FNameList = loadTable('./resources/csv/FirstNames.csv', 'header');
+  LNameList = loadTable('./resources/csv/LastNames.csv', 'header');
+  Conthrax = loadFont('./resources/fonts/conthrax-sb.ttf');
+  SofachromeIt = loadFont('./resources/fonts/sofachrome rg it.ttf');
+  Sofachrome = loadFont('./resources/fonts/sofachrome rg.ttf');
+}
+
+
 function setup(){
-  SWidth = screen.availWidth;
-  SHeight = screen.availHeight;
+  SWidth = window.innerWidth;
+  SHeight = window.innerHeight;
+  FName = "";
+  LName = "";
   createCanvas(SWidth, SHeight);
+  textAlign(CENTER);
+  rectMode(CENTER);
 }
 
 function draw(){
