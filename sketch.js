1ed39dcb0c08dcd4ff4ef040c34e6da32d864e62
Goosy-Goose <35647530+Goosy-Goose@users.noreply.github.com>
2022-12-05 13:07:32 -0500

    yeah lol

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 var SWidth, SHeight;
 var FNameList, LNameList;
 var Conthrax, SofachromeIt, Sofachrome;
-var FName, LName;
+var FName, LName, UserName;
 var StartPts;
 
 
 function preload(){
-  FNameList = loadTable('./resources/csv/FirstNames.csv', 'header');
-  LNameList = loadTable('./resources/csv/LastNames.csv', 'header');
+  FNameList = loadTable('./resources/csv/FirstNames.csv', 'csv', 'header');
+  LNameList = loadTable('./resources/csv/LastNames.csv', 'csv', 'header');
   Conthrax = loadFont('./resources/fonts/conthrax-sb.ttf');
   SofachromeIt = loadFont('./resources/fonts/sofachrome rg it.ttf');
   Sofachrome = loadFont('./resources/fonts/sofachrome rg.ttf');
@@ -17,8 +17,7 @@ function preload(){
 function setup(){
   SWidth = window.innerWidth;
   SHeight = window.innerHeight;
-  FName = "";
-  LName = "";
+  UserName = makeUserName();
   createCanvas(SWidth, SHeight);
   textAlign(CENTER);
   rectMode(CENTER);
@@ -26,5 +25,18 @@ function setup(){
 
 function draw(){
   background(220);
+  push();
+  textFont(Conthrax);
+  textSize(36);
+  text(UserName, window.innerWidth/2, window.innerHeight/2)
+  pop();
+}
 
+function makeUserName(){
+  let fNameInt = floor(random(0, FNameList.getRowCount()));
+  let lNameInt = floor(random(0, LNameList.getRowCount()));
+  FName = FNameList.getColumn('Name')[fNameInt];
+  LName = LNameList.getColumn('name')[lNameInt].toLowerCase();
+  LName = LName.charAt(0).toUpperCase() + LName.substring(1).toLowerCase();
+  return FName + " " + LName;
 }
